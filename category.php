6272bcc6c52eff6317eb141ad0745c2b17b93e2f
Pasindu <pasindusangeeth968@gmail.com>
2024-12-25 18:49:48 +0530

    category added

---
+++ category.php
@@ -0,0 +1,121 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title></title>
+    <link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic" rel="stylesheet" type="text/css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script>
+        window.addEventListener("load", function() {
+            window.scrollTo(0, 1);
+        });
+    </script>
+    <!--js-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!--/js-->
+</head>
+
+<body style="" data-new-gr-c-s-check-loaded="14.1215.0" data-gr-ext-installed="">
+
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title></title>
+
+
+
+
+<div class="header-top">
+    <!--container-->
+    <div class="container">
+        <div class="top-nav">
+            <div class="logo">
+                <a href="#"><img src="images/" id="section-1" class="img-responsive" alt=""></a>
+            </div>
+            <div class="menu">
+                <ul id="nav">
+                    <li><a href="index.php#section-1" onclick="javascript:window.open('index.php#section-1','_self')">Home</a></li>
+                    <li><a href="index.php#section-2" onclick="javascript:window.open('index.php#section-2','_self')">About</a></li>
+                    <li><a href="index.php#section-3" onclick="javascript:window.open('index.php#section-3','_self')">Gallery</a></li>
+
+                    <li><a href="index.php#section-5" onclick="javascript:window.open('index.php#section-5','_self')">Contact</a></li>
+                    <li><a href="category.php" onclick="javascript:window.open('category.php','_self')">Category</a></li>
+
+                    <li><a href="#" onclick="javascript:window.open('index.php#section-1','_self')">Admin Login</a></li>
+                    <div class="clearfix"></div>
+                </ul>
+            </div>
+        </div>
+        <div class="clearfix"> </div>
+
+
+    </div>
+    <!--/container-->
+</div><!--/sticky-->
+<br>
+<br>
+<br>
+<div style="height:50px"></div>
+<div style="width:1000px; margin:auto">
+
+    <div style="width:200px; float:left">
+
+        <table cellpadding="0" cellspacing="0" width="1000px">
+            <tbody><tr><td style="font-size:30px; color:#09F"><b>Category</b></td></tr>
+            <tr><td style=" padding:5px;"><b><a href="subcat.php?catid=1">Family Tours</a></b></td></tr><tr><td style=" padding:5px;"><b><a href="subcat.php?catid=2">Religious Tours</a></b></td></tr><tr><td style=" padding:5px;"><b><a href="subcat.php?catid=3">Adventure Tours</a></b></td></tr><tr><td style=" padding:5px;"><b><a href="subcat.php?catid=4">Special Event Tours</a></b></td></tr><tr><td style=" padding:5px;"><b><a href="subcat.php?catid=5">National Parks</a></b></td></tr><tr><td style=" padding:5px;"><b><a href="subcat.php?catid=6">Themed Vacations</a></b></td></tr><tr><td style=" padding:5px;"><b><a href="subcat.php?catid=7">Small Group Tours</a></b></td></tr>
+            </tbody></table>
+
+    </div>
+
+    <div style="width:500px; float:left">
+        <table cellpadding="0px" cellspacing="0" width="1000px">
+            <tbody><tr><td class="headingText">Welcome to Go Travel &amp; Tourism Ltd</td></tr>
+            <tr><td class="paraText" width="900px">Tourism is a powerful engine for economic growth, job creation, and cultural exchange. It brings in foreign currency, boosts local businesses, and creates diverse employment opportunities. By experiencing new cultures and perspectives, travelers gain a deeper understanding of the world, fostering global citizenship and tolerance. Tourism also incentivizes the preservation of cultural heritage sites and traditions, ensuring their protection for future generations. Furthermore, sustainable tourism practices can contribute to environmental conservation efforts and promote responsible travel. Overall, tourism plays a vital role in building a more interconnected, prosperous, and culturally rich world.
+                </td><td style="background-image:url(); background-repeat:no-repeat; color:#FFF; font-family:Lucida Sans Unicode, Lucida Grande, sans-serif; font-size:24px; " width="500px" height="150px">&lt; &nbsp;&nbsp;&nbsp; </td></tr></tbody></table>
+
+    </div>
+
+</div>
+
+<div style="clear:both"></div>
+
+<div class="fotter">
+    <div class="container">
+        <div class="fotter-grids">
+            <div class="col-md-4 fotter-left">
+                <p style="text-align:justify"></p>
+            </div>
+            <div class="col-md-4 fotter-middle">
+                <h3></h3>
+                <div class="footer-list">
+                    <h3></h3>
+                    <div class="footer-list">
+                    </div>
+
+                </div>
+                <div class="col-md-4 fotter-right" style="padding-left:10px">
+                    <h3></h3><br>
+                    <div class="footer-list">
+                    </div></div>
+                <div class="social-icons">
+                    <a href="#"><span class="facebook"> </span></a>
+                    <a href="#"><span class="twitter"> </span></a>
+                    <a href="#"><span class="googleplus"> </span></a>
+                    <a href="#"><span class="pinterest"> </span></a>
+                    <a href="#"><span class="instagram"> </span></a>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
+<div class="copyright text-right">
+    <p style="padding-right:100px"></p>
+</div>
+
+
+<daily-companion-app></daily-companion-app></body><grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration></html>

36edc1e2c1e4ecc605c94d34c03152c69c094486
Pasindu <pasindusangeeth968@gmail.com>
2024-12-25 10:51:49 +0530

    bootstrap,css files linked to index. eventlistner added.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script>
+        window.addEventListener("load", function() {
+            window.scrollTo(0, 1);
+        });
+    </script>
+
+</head>
+</html>

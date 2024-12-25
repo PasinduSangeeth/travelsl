5d2e98576af31e03314798e47a2a1e3c7ea39683
Pasindu <pasindusangeeth968@gmail.com>
2024-12-25 15:26:11 +0530

    style css & bootstrap done

--- index.php
+++ index.php
@@ -12,8 +12,165 @@
     </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
-
 <body>
+<div id="section-1" class="section">
+    <div id="top" class="callbacks_container">
+        <ul class="rslides" id="slider4">
+            <li>
+                <img src="images/Sri.jpg" alt="">
+                <div class="caption">
+                    <div class="header-info">
+                        <h2><a href="#">Your Trusted Travel Packages and Services</a></h2>
+                        <lable></lable>
+                        <h1><a href="#">Go Travel & Tourism Ltd </a></h1>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="clearfix">
+    </div>
+</div>
+<div class="about section" id="section-2">
+    <div class="about-head text-center">
+        <h3>About Us</h3>
+    </div>
+    <div class="container">
+        <div class="col-md-4 about-grids">
+            <h4>Our Vision</h4>
+            <p>Pain itself is love, the main architect of pain, but at times it occurs in which toil and pain may bring some great pleasure. For example, some people, by pursuing pleasure, avoid certain obligations or endure pain for its benefits. However, these pains may result in unseemly consequences if one doesn't exercise reason and discernment. Thus, any such labor or discomfort is excused as an escape from accusations or harsh consequences of the harsh realities of the soul.</p>
+        </div>
+        <div class="col-md-4 about-grids grid2">
+            <h4>Our Mission</h4>
+            <p>True fulfillment often arises from overcoming challenges and enduring hardships, as they pave the way for greater satisfaction. Many seek pleasure and comfort, yet sometimes the pursuit of these goals requires effort and sacrifice. However, without careful thought, this pursuit may lead to unintended consequences. In such cases, the hardships faced are often justified as necessary steps to avoid greater struggles or discomforts, offering a reprieve from more significant burdens.</p>
+        </div>
+        <div class="col-md-4 about-grids">
+            <h4>Safety Information</h4>
+            <p>Safety, like the pursuit of comfort, often requires effort and the willingness to endure certain challenges. Just as pain can lead to growth, adhering to safety protocols might sometimes feel inconvenient, but it ensures greater protection in the long run. While it may seem tempting to bypass certain precautions for immediate ease, doing so can lead to serious consequences. By making safety a priority, we avoid potential risks and ensure a secure environment for everyone involved. In this way, safety is not just a set of rules, but a conscious decision to protect ourselves and others from harm..</p>
+        </div>
+    </div>
+    <!--top-tours-->
+    <div  class="section" id="section-3">
+        <div id="portfolio" class="portfolio">
+            <div class="top-tours-head text-center">
+                <h3>Gallery</h3>
+            </div>
+            <ul id="filters" class="clearfix wow bounceIn" data-wow-delay="0.4s">
+                <li><span class="filter active" data-filter="app card icon logo fun">ALL</span></li>
+                <li><span class="filter" data-filter="app">Domestic Travel</span></li>
+                <li><span class="filter" data-filter="card">Foreign Travel</span></li>
+                <li><span class="filter" data-filter="icon">Short Date Tour</span></li>
+                <li><span class="filter" data-filter="fun">Long Date Tour</span></li>
+            </ul>
+            <div id="portfoliolist">
+                <div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
+                    <div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
+                        <a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+                            <img src="images/t1.jpg" class="img-responsive" alt=""/></a>
+                    </div>
+                </div>
+                <div class="portfolio app mix_all" data-cat="app" style="display: inline-block; opacity: 1;">
+                    <div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
+                        <a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+                            <img src="images/t2.jpg" class="img-responsive" alt=""/></a>
+                    </div>
+                </div>
+                <div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
+                    <div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
+                        <a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+                            <img src="images/t3.jpg" class="img-responsive" alt=""/></a>
+
+                    </div>
+                </div>
+                <div class="portfolio icon mix_all" data-cat="icon" style="display: inline-block; opacity: 1;">
+                    <div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
+                        <a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+                            <img src="images/t4.jpg" class="img-responsive" alt=""/></a>
+
+                    </div>
+                </div>
+                <div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
+                    <div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
+                        <a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+                            <img src="images/t5.jpg" class="img-responsive" alt=""/></a>
+
+                    </div>
+                </div>
+                <div class="portfolio fun mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
+                    <div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
+                        <a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+                            <img src="images/t6.jpg" class="img-responsive" alt=""/></a>
+
+                    </div>
+                </div>
+                <div class="portfolio fun mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
+                    <div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
+                        <a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+                            <img src="images/t7.jpg" class="img-responsive" alt=""/></a>
+
+                    </div>
+                </div>
+                <div class="portfolio icon mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
+                    <div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">
+                        <a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+                            <img src="images/t8.jpg" class="img-responsive" alt=""/></a>
+
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+    </div>
+
+    <!--/top-tours-->
+    <div id="section-5" class="contact section">
+        <div class="contact-head text-center">
+            <h3>Contact Us</h3>
+            <br>
+            <h4 style="color:#000">Plan Your Trip</h4>
+            <br>
+            <h5 style="color:#000">Our travel experts can help you book now</h5>
+
+            <div class="contact-grids">
+                <div class="container">
+                    <div class="col-md-4 address">
+                        <h4 style="color:#09F"></h4>
+                        <p style="color:#000"><br/>
+                        </p>
+                        <br/>
+                    </div>
+                    <div class="col-md-8 contact">
+
+                        <?php
+                        if(isset($_POST["sbmt"]))
+                        {
+                            $cn=makeconnection();
+                            $s="insert into contactus(Name,Phno,Email,Message) values('" . $_POST["t1"] ."','" . $_POST["t2"] ."','" . $_POST["t3"] ."','" . $_POST["t4"] ."')";
+                            mysqli_query($cn,$s);
+                            mysqli_close($cn);
+                            echo "<script>alert('Record Save');</script>";
+                        }
+                        ?>
+                        <form method="post">
+                            <table border="0" width="700px" height="500px">
+                                <tr><td align="left"> <input type="text" class="text" value=" Name"  name="t1" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your Name';}" required pattern="[a-zA-z1 _]{1,50}" title"Please Enter Only Characters and numbers between 1 to 50 for Name"></td></tr>
+                                <tr><td align="left"><input type="text" class="text" value=" Contact No" name="t2" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your Contact NO';}" required pattern="[0-9]{10,12}" title"Please Enter Only  numbers between 10 to 12 for Contact no"></td></tr>
+                                <tr><td align="left"> <input type="text" class="text" value="Email" name="t3" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your mail';}" required></td></tr>
+                                <tr><td><textarea onFocus="if(this.value == 'Message') this.value='';" name="t4" onBlur="if(this.value == '') this.value='Enter Message Here';" required/ >Message</textarea></td></tr>
+                                <tr><td><input type="submit" value="Send message" name="sbmt"></td></tr></table>
+                            <div class="clearfix"></div>
+                        </form>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+
+
+
+            <?php include('bottom.php'); ?>
+</body>
+</html>
+
 
 </body>
 </html>
